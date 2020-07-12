84119a14b002577b59b684b5b2b3224da450eb75
Maya Raman <maya.raman19@gmail.com>
2020-07-11 21:44:38 -0700

    added dumb css and useless js file

---
+++ index.js
@@ -0,0 +1,7 @@
+let rand_facts = [
+    "I am an Aries sun, Pisces moon. Do with that what you will.",
+    "My least favorite food is mushrooms. I am, however, starting to tolerate it.",
+    "I used to exclusively lucid dream, but lost the ability when I went to college???"
+]
+
+document.getElementById("rand-fact").innerHTML = rand_facts[1];
