0fdfba715e9c144ebbbd486c34c7a7c38db954b1
Maya Raman <maya.raman19@gmail.com>
2020-07-15 01:58:11 -0700

    I am Attempting

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+//background
+
+let background = document.getElementById("more-info");
+
+window.addEventListener('scroll', () => {
+    /*let y = window.scrollY;
+    let height = background.clientHeight * 0.2;
+    if (y > height) {
+        background.style.display = 'none';
+    }
+    else {
+        background.style.display = 'block';
+    }*/
+    let y = background.offsetHeight;
+    let height = background.clientHeight * 0.5;
+    if (y > height) {
+        background.scrollTop = 0;
+    }
+
+    
+});
+
+//random fact
+
 let rand_facts = [
     "I am an Aries sun, Pisces moon. Do with that what you will.",
     "I despise mushrooms more than any other food. I am, however, starting to tolerate them.",
     "I used to exclusively lucid dream, but lost the ability when I went to college???"
 ]
-
 
 let index=Math.round((Math.random() * 2));
 
