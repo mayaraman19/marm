22a4bb077b5428a32f9f5144b4e310ef9997bcbe
Maya Raman <maya.raman19@gmail.com>
2020-07-17 16:41:31 -0700

    don't know what's going on but it works

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ showLink = (link) => {
         document.getElementById("medium").style.display = "flex";
         document.getElementById("gitlink").style.display = "none";
         document.getElementById("resume").style.display = "none";
+        
     } else if (link === "work") {
         document.getElementById("medium").style.display = "none";
         document.getElementById("gitlink").style.display = "flex";
