aefac78a5f2de687517395bb04f15b8e791e57b1
Maya Raman <maya.raman19@gmail.com>
2020-07-16 20:43:44 -0700

    buttons working

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 
-showMedium = () => {
-    let medium = document.getElementById("medium");
-    if(medium.style.display === "none")
-        medium.style.display = "block";
-    else medium.style.display = "none";
+showLink = (link) => {
+    //link is either friend, work, boss
+    if (link === "friend") {
+        document.getElementById("medium").style.display = "flex";
+        document.getElementById("gitlink").style.display = "none";
+        document.getElementById("resume").style.display = "none";
+    } else if (link === "work") {
+        document.getElementById("medium").style.display = "none";
+        document.getElementById("gitlink").style.display = "flex";
+        document.getElementById("resume").style.display = "none";
+    } else if (link === "boss") {
+        document.getElementById("medium").style.display = "none";
+        document.getElementById("gitlink").style.display = "none";
+        document.getElementById("resume").style.display = "flex";
+    }
+
 }
